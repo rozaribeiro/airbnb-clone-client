9ec8c7e55c417ce1ba8b71489694ea20c62bfcbe
rozaribeiro <rozaribeiro@gmail.com>
2021-05-07 15:43:54 +0200

    signin wip

--- src/pages/Authenticate/SignInPage.jsx
+++ src/pages/Authenticate/SignInPage.jsx
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 // import api from
 // import {BrowserRouter as Router, Switch, Route, Redirect, Link} from 'react-router-dom';
 import { BrowserRouter as Link } from "react-router-dom";
+import { userService } from "../../services/"
 
 export default class SignInForm extends Component {
   constructor(props) {
@@ -23,20 +24,22 @@ export default class SignInForm extends Component {
     console.log("password", this.state.password);
   };
 
-  handleSubmit() {
-    // api.post()
-    // if (error) {
-    // console.log(error);
-    // }
-    // else {
-    //   console.log("else");
-    //   // localStorage
-    // }
-  }
+  handleClick = async (e) => {
+    const {email, password} = this.state;
+
+    try {
+        const response = await userService.login(email, password);
+        localStorage.setItem('token', response.data.token);
+        // this.context.setAuth(true);
+        this.props.history.push('/home');
+    } catch(error) {
+        this.setState({error: error.response.data.error});
+    }
+}
   render() {
     return (
-      <div>
-        {/* <h1>Sign In</h1>
+      <>
+        <h1>Sign In</h1>
         <form action="">
           <label htmlFor="email">Email</label>
           <input
@@ -55,8 +58,8 @@ export default class SignInForm extends Component {
           <button onClick={this.handleSubmit}></button>
           <Link>Forgot Password?</Link>
           <Link>Forgot Email?</Link>
-        </form> */}
-      </div>
+        </form>
+      </>
     );
   }
 }
--- src/services/user.js
+++ src/services/user.js
@@ -1,16 +1,19 @@
 import api from "./api";
 
 const userService = {
-  // login: async (email, password) => {
-  //  const user = {email, password};
 
-  //     return await api.post('/api/signin', user);
-  // },
   signup: async (first_name, last_name, role, email, password) => {
     const user = { first_name, last_name, role, email, password };
 
     return await api.post("/api/signup", user);
   },
+  
+  signin: async (email, password) => {
+    const user = { email, password };
+
+    return await api.post("/api/signin", user);
+  },
+
   // other service method
 };
 
