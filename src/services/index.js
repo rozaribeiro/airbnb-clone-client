fe6c53381dca54b37743982ad0279e9ff5ca2ede
dv-kong <zektbach@outlook.com>
2021-05-09 17:27:12 +0200

    services fix

--- src/services/index.js
+++ src/services/index.js
@@ -1,10 +1,5 @@
 import userService from "./user";
-<<<<<<< HEAD
-import bookingsService from "./bookings";
-export { userService, bookingsService };
-=======
 import availablePLaceService from "./place";
 import bookingsService from "./bookings";
 
 export { userService, availablePLaceService, bookingsService };
->>>>>>> 44c746ff8bc0dbf66349e49842564fa0b83c7a6f
