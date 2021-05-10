a476c38c9a2d69997febbd93353630c8cfe784aa
nawalpro <nawal.maizate@gmail.com>
2021-05-10 10:01:24 +0200

    Bookplace ip

--- src/pages/BookPlace/index.js
+++ src/pages/BookPlace/index.js
@@ -1,42 +1,43 @@
-import React, { Component } from "react";
-import appContext from "../../store";
-import { bookingsService } from "../../services"
+// import React, { Component } from "react";
+// import appContext from "../../store";
+// import { bookingsService } from "../../services"
 
-export default class index extends Component {
-  constructor(props) {
-    super(props)
+// export default class index extends Component {
+//   constructor(props) {
+//     super(props)
     
-    this.state = {
-        data: [],
-        error: null
-    };
-}
+//     this.state = {
+//         data: [],
+//         error: null
+//     };
+// }
 
-async componentDidMount() {
-    try {
-        const response = await bookingsService.bookPlaces();
-        this.setState({ data: response.data });
-        console.log("DATA BOOK PLACE",response.data);
-    } catch (e) {
-        if (e.response.status === 403) {
-            localStorage.removeItem('token');
-            this.props.history.push('/');
-            this.context.setAuth(false);
-        }
-        this.setState({ error: e.message });
-    }
-}
-  render() {
-    return (
+// async componentDidMount() {
+//     try {
+//         const response = await bookingsService.bookPlaces();
+//         this.setState({ data: response.data });
+//         console.log("DATA BOOK PLACE",response.data);
+//     } catch (e) {
+//         if (e.response.status === 403) {
+//             localStorage.removeItem('token');
+//             this.props.history.push('/');
+//             this.context.setAuth(false);
+//         }
+//         this.setState({ error: e.message });
+//     }
+// }
+//   render() {
+//     return (
       
-      <div>
-        {this.state.data.map((elelement, index) => {
-          return( 
-            <button>Book Place</button>
-          )
+//       <div>
+//         {this.state.data.map((elelement, index) => {
+//           return( 
             
-       )}
-    </div>
-    );
-  }
-}
+//             <button>Book Place</button>
+//           )
+            
+//        )}
+//     </div>
+//     );
+//   }
+// }
